afd9d0d373eeae2b8ad9e3e670135819525261d8
Atul <atulksingh.15@gmail.com>
2024-11-15 21:47:00 +0530

    3rd

--- src/test/java/elements/LoginPage.java
+++ src/test/java/elements/LoginPage.java
@@ -1,7 +1,15 @@
 package elements;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 public class LoginPage {
-	
-	public String baseUrl="https://automationexercise.com/login";
+
+
+	public String baseUrl="https://automationexercise.com/";
+	public	By pageverify =By.linkText("Home");
+    public By signup =By.xpath("//a[@href='/login']");
+
+
 
 }
--- src/test/java/testCases/TestCases.java
+++ src/test/java/testCases/TestCases.java
@@ -2,8 +2,11 @@ package testCases;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -19,22 +22,33 @@ public class TestCases {
 	public void setup() {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-
-
 	}
 	@Test
-	public void registerUser() {
+	public void registerUser() throws InterruptedException {
+
+
 		driver.get(config.baseUrl);
+		Thread.sleep(5000);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
-		
+		//	3. Verify that home page is visible successfully
+		WebElement element = driver.findElement(config.pageverify);
+		String actualText = element.getText();
+		// The expected text
+		String expectedText = "Home";
+		// Verify that the text matches
+		Assert.assertEquals(actualText, expectedText, "The text on the page does not match the expected value.");
+		Thread.sleep(5000);
+		//4. Click on 'Signup / Login' button
+		driver.findElement(config.signup).click();
+		//5. Verify 'New User Signup!' is visible
 
 	}
-	
-@AfterTest
-public void down() {
-	driver.close();
-	driver.quit();
-}
+
+	@AfterTest
+	public void down() {
+		driver.close();
+		driver.quit();
+	}
 
 
 }
