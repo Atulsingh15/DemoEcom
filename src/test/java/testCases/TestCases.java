255af95a7d9e20dc372fcbaf6e227c6cc0928da9
Atul <atulksingh.15@gmail.com>
2024-11-15 20:31:37 +0530

    new

--- src/test/java/testCases/TestCases.java
+++ src/test/java/testCases/TestCases.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -12,24 +13,28 @@ import elements.LoginPage;
 
 public class TestCases {
 	public WebDriver driver;
-	
-@BeforeTest
-public void setup() {
-	
-driver = new ChromeDriver();
-driver.manage().window().maximize();
-
-	
-}
-@Test
-public void registerUser() {
 	LoginPage config = new LoginPage();
-	driver.get(config.baseUrl);
-	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
 
+	@BeforeTest
+	public void setup() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+
+	}
+	@Test
+	public void registerUser() {
+		driver.get(config.baseUrl);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+		
+
+	}
+	
+@AfterTest
+public void down() {
+	driver.close();
+	driver.quit();
 }
-	
-	
-	
-	
+
+
 }
