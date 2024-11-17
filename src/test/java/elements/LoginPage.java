5a12f72c37697c4fce630901723e03ddb7b80d80
Atul <atulksingh.15@gmail.com>
2024-11-17 15:03:56 +0530

    7 cases done

--- src/test/java/elements/LoginPage.java
+++ src/test/java/elements/LoginPage.java
@@ -1,15 +1,28 @@
 package elements;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class LoginPage {
 
-
+	public WebDriver driver;
 	public String baseUrl="https://automationexercise.com/";
 	public	By pageverify =By.linkText("Home");
     public By signup =By.xpath("//a[@href='/login']");
-
-
+    
+     
+    public By signuppage =By.xpath("//h2[text()='Login to your account']");
+    public By signname= By.xpath("//input[@data-qa='signup-name']");
+    public By signamail=By.xpath("//input[@data-qa='signup-email']");
+    
+    public By signbutton=By.xpath("//button[@data-qa='signup-button']");
+    
+//    public void EnterName(String name) {
+//		driver.findElement(signname);
+//		
+//	}
+    
+    
 
 }
--- src/test/java/testCases/TestCases.java
+++ src/test/java/testCases/TestCases.java
@@ -1,6 +1,9 @@
 package testCases;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -13,10 +16,10 @@ import org.testng.annotations.Test;
 
 import elements.LoginPage;
 
-test
 public class TestCases {
 	public WebDriver driver;
 	LoginPage config = new LoginPage();
+	LoginPage login;
 
 	@BeforeTest
 	public void setup() {
@@ -24,9 +27,13 @@ public class TestCases {
 		driver.manage().window().maximize();
 	}
 	@Test
-	public void registerUser() throws InterruptedException {
+	public void registerUser() throws InterruptedException, IOException {
 
-
+		Properties prop =new Properties();
+		FileInputStream fis = new FileInputStream("C:\\Users\\dell\\Desktop\\selenium\\DemoEcom\\src\\test\\resources\\resources.properties");
+		prop.load(fis);
+		String name = prop.getProperty("name");
+		String mail = prop.getProperty("mail");
 		driver.get(config.baseUrl);
 		Thread.sleep(5000);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
@@ -37,10 +44,23 @@ public class TestCases {
 		String expectedText = "Home";
 		// Verify that the text matches
 		Assert.assertEquals(actualText, expectedText, "The text on the page does not match the expected value.");
-		Thread.sleep(5000);
 		//4. Click on 'Signup / Login' button
 		driver.findElement(config.signup).click();
 		//5. Verify 'New User Signup!' is visible
+		WebElement signup =driver.findElement(config.signuppage);
+		String actual =signup.getText();
+		String expected ="Login to your account";
+		Assert.assertEquals(actual, expected, "The text on the page does not match the expected value.");
+
+		//6. Enter name and email address
+		driver.findElement(config.signname).sendKeys(name);
+		driver.findElement(config.signamail).sendKeys(mail);
+		Thread.sleep(5000);	
+		//7. Click 'Signup' button
+		driver.findElement(config.signbutton).click();
+		//8. Verify that 'ENTER ACCOUNT INFORMATION' is visible
+		
+
 
 	}
 
