751d4e6e724f9787cdd34321fdd898177f432225
Atul <atulksingh.15@gmail.com>
2025-01-20 19:17:50 +0530

    scenerio 1 done

--- src/test/java/elements/LoginPage.java
+++ src/test/java/elements/LoginPage.java
@@ -9,20 +9,38 @@ public class LoginPage {
 	public WebDriver driver;
 	public String baseUrl="https://automationexercise.com/";
 	public	By pageverify =By.linkText("Home");
-    public By signup =By.xpath("//a[@href='/login']");
-    
-     
-    public By signuppage =By.xpath("//h2[text()='Login to your account']");
-    public By signname= By.xpath("//input[@data-qa='signup-name']");
-    public By signamail=By.xpath("//input[@data-qa='signup-email']");
-    
-    public By signbutton=By.xpath("//button[@data-qa='signup-button']");
-    
-//    public void EnterName(String name) {
-//		driver.findElement(signname);
-//		
-//	}
-    
-    
+	public By signup =By.xpath("//a[@href='/login']");
+	public By veraccounts =By.xpath("//b[text()='Address Information']");
+	public By signuppage =By.xpath("//h2[text()='Login to your account']");
+	public By signname= By.xpath("//input[@data-qa='signup-name']");
+	public By signamail=By.xpath("//input[@data-qa='signup-email']");
+	public By signbutton=By.xpath("//button[@data-qa='signup-button']");
+	public By radiobutton=By.id("id_gender1");
+	public By password=By.xpath("//input[@id='password']");
+	public By days =By.id("days");
+	public By month =By.id("months");
+	public By year =By.id("years");
+	public By firstname =By.id("first_name");
+	public By lastname =By.id("last_name");
+	public By company =By.id("company");
+	public By address = By.id("address1");
+	public By country =By.id("country");
+	public By state =By.id("state");
+	public By city =By.id("city");
+	public By zipcode=By.id("zipcode");
+	public By mobilenumber=By.id("mobile_number");
+	public By newsletter=By.id("newsletter");
+	public By optin =By.id("optin");
+	public By createaccount=By.xpath("//button[normalize-space()='Create Account']");
+	public By verifyaccount=By.linkText("//b[text()='Account Created!']");
+	public By continu=By.xpath("//a[normalize-space()='Continue']");
+	public By delete=By.xpath("//a[text()=' Delete Account']");
+
+	//    public void EnterName(String name) {
+	//		driver.findElement(signname);
+	//		
+	//	}
+
+
 
 }
--- src/test/java/testCases/TestCases.java
+++ src/test/java/testCases/TestCases.java
@@ -6,9 +6,12 @@ import java.time.Duration;
 import java.util.Properties;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -59,7 +62,59 @@ public class TestCases {
 		//7. Click 'Signup' button
 		driver.findElement(config.signbutton).click();
 		//8. Verify that 'ENTER ACCOUNT INFORMATION' is visible
+		WebElement veraccount =driver.findElement(config.veraccounts);
+		String actualver=veraccount.getText();
+		String expectedver = "ADDRESS INFORMATION";
+		Assert.assertEquals(actualver, expectedver, "The text on the page does not match the expected value.");
+
+		//9. Fill details: Title, Name, Email, Password, Date of birth
+
+		driver.findElement(config.radiobutton).click();
+		driver.findElement(config.password).sendKeys("arjun");
+		Thread.sleep(5000);
+		WebElement days =driver.findElement(config.days);
+		Select day1=new Select(days);
+		day1.selectByValue("1");
+		WebElement month =driver.findElement(config.month);
+		Select month1=new Select(month);
+		month1.selectByValue("1");
+		WebElement year =driver.findElement(config.year);
+		Select year1=new Select(year);
+		year1.selectByValue("2021");
+		Actions actions = new Actions(driver);
+		driver.findElement(config.newsletter).click();
+		driver.findElement(config.optin).click();
+
+		actions.sendKeys(Keys.PAGE_DOWN).perform();
+		driver.findElement(config.firstname).sendKeys("arjun");;
+		driver.findElement(config.lastname).sendKeys("singh");
+		driver.findElement(config.company).sendKeys("edunext");
+		driver.findElement(config.address).sendKeys("noida");
+		WebElement country =driver.findElement(config.country);
+		Select country1=new Select(country);
+		country1.selectByValue("India");
+		driver.findElement(config.state).sendKeys("Up");
+		driver.findElement(config.city).sendKeys("GBN");
+		driver.findElement(config.zipcode).sendKeys("898989");
+		driver.findElement(config.mobilenumber).sendKeys("1111111111");
+		driver.findElement(config.createaccount).click();
+		//14. Verify that 'ACCOUNT CREATED!' is visible;
+		//WebElement verifyaccounts = driver.findElement(config.verifyaccount);
 		
+		//String expect="Account Created!";
+		//String actu= verifyaccounts.getText();
+
+	//Assert.assertEquals(actu, expect, "The text on the page does not match the expected value.");
+		
+		//15. Click 'Continue' button
+		driver.findElement(config.continu).click();
+		
+		//16. Verify that 'Logged in as username' is visible
+		
+		//17. Click 'Delete Account' button
+		driver.findElement(config.delete).click();
+		//18. Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button
+
 
 
 	}
